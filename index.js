4fb8a9e371454734f24a0c72c9b4ebaa81fa5aa2
Prashant2raja <144902971+Prashant2raja@users.noreply.github.com>
2024-01-07 17:29:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ let image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 wins 🚩";
+  document.querySelector("h1").innerHTML = "  🚩 Player 1 wins";
 } else if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 2 wins 🚩";
+  document.querySelector("h1").innerHTML = " Player 2 wins 🚩";
 } else {
   document.querySelector("h1").innerHTML = "Draw !";
 }
